168089d995aacc8aca15a256e05512cd4afba843
Ireneusz Patalas <fido@list.pl>
2024-06-04 09:09:34 +0200

    Automatic changelog when creating a release

---
+++ extract-changelog.ps1
@@ -0,0 +1,33 @@
+$version = $args[0]
+
+if (-not $version) {
+	Write-Output "Usage: extract-changelog.ps1 <version>"
+	exit 1
+}
+
+$output = ""
+$found = $false
+
+foreach ($line in Get-Content -Path CHANGELOG.md) {
+	if ($found -and $line -match "^## \[") {
+		break
+	}
+
+	if ($found) {
+		$output += $line + "`n"
+	}
+
+	if (!$found -and $line -match "^## \[$version\]") {
+		$found = $true
+		continue
+	}
+}
+
+$output = $output.TrimEnd()
+
+if ($output.Length -eq 0) {
+	Write-Output "No changelog found for version $version"
+	exit 1
+}
+
+Write-Output $output
