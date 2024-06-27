c6fe267692ea7da999e9481d6316910bde7ee137
Ireneusz Patalas <fido@list.pl>
2024-06-27 22:48:17 +0200

    Fix setting foreground window to make it work not only in the debugger mode ;)

--- AppSwitcher/Switcher.cs
+++ AppSwitcher/Switcher.cs
@@ -3,8 +3,10 @@ using AppSwitcher.WindowDiscovery;
 using Microsoft.Extensions.Logging;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.WindowsAndMessaging;
+using Windows.Win32.UI.Input.KeyboardAndMouse;
 using Windows.Win32;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace AppSwitcher;
 
@@ -84,13 +86,49 @@ internal class Switcher
     private void ActivateWindow(ApplicationWindow window)
     {
         var hwnd = window.Handle;
-        PInvoke.ShowWindow(hwnd, SHOW_WINDOW_CMD.SW_RESTORE);
+        if (PInvoke.IsIconic(hwnd))
+        {
+            PInvoke.ShowWindow(hwnd, SHOW_WINDOW_CMD.SW_RESTORE);
+        }
+
+        // https://github.com/microsoft/PowerToys/blob/8b8c75b9a54a342660ce8475989ef283fe39e8fe/src/common/ManagedCommon/WindowHelpers.cs#L11
+        INPUT input = new() { type = INPUT_TYPE.INPUT_MOUSE, Anonymous = new() { mi = new() } };
+        INPUT[] inputs = [input];
+        PInvoke.SendInput(inputs, Marshal.SizeOf(typeof(INPUT)));
+
         PInvoke.SetForegroundWindow(hwnd);
+
+        var fgHandle = PInvoke.GetForegroundWindow();
+
+        if (fgHandle != hwnd)
+        {
+            var threadId1 = GetWindowThreadProcessId(hwnd);
+            var threadId2 = GetWindowThreadProcessId(fgHandle);
+
+            if (threadId1 != threadId2)
+            {
+                PInvoke.AttachThreadInput(threadId1, threadId2, true);
+                PInvoke.SetForegroundWindow(hwnd);
+                PInvoke.AttachThreadInput(threadId1, threadId2, false);
+            }
+            else
+            {
+                PInvoke.SetForegroundWindow(hwnd);
+            }
+        }
     }
 
     private void HideWindow(ApplicationWindow window)
     {
         var hwnd = window.Handle;
         PInvoke.ShowWindow(hwnd, SHOW_WINDOW_CMD.SW_MINIMIZE);
+    }
+
+    internal unsafe static uint GetWindowThreadProcessId(HWND hwnd)
+    {
+        unsafe
+        {
+            return PInvoke.GetWindowThreadProcessId(hwnd, null);
+        }
     }
 }
