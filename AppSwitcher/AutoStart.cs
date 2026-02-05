86e85937996b9831f586a8228730a2b89d5b05ba
Ireneusz Patalas <fido@list.pl>
2026-02-05 10:17:31 +0100

    Replace COM calls to create shortcut link with native calls

--- AppSwitcher/AutoStart.cs
+++ AppSwitcher/AutoStart.cs
@@ -1,38 +1,105 @@
-﻿using IWshRuntimeLibrary;
 using Microsoft.Extensions.Logging;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace AppSwitcher;
-internal class AutoStart
+
+internal class AutoStart(ILogger<AutoStart> logger)
 {
-    private readonly ILogger<AutoStart> _logger;
-
-    public AutoStart(ILogger<AutoStart> logger)
-    {
-        _logger = logger;
-    }
-
     public bool CreateShortcut()
     {
         var executablePath = Environment.ProcessPath;
         if (executablePath is null)
         {
-            _logger.LogError("Cannot get current process executable path.");
+            logger.LogError("Cannot get current process executable path.");
             return false;
         }
         var startupPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
         var fileName = Path.GetFileNameWithoutExtension(executablePath);
         var originalFileDirectory = Path.GetDirectoryName(executablePath);
 
-        string link = Path.Combine(startupPath, fileName + ".lnk");
-        var shell = new WshShell();
-        if (shell.CreateShortcut(link) is IWshShortcut shortcut)
+        if (string.IsNullOrEmpty(originalFileDirectory))
         {
-            shortcut.TargetPath = executablePath;
-            shortcut.WorkingDirectory = originalFileDirectory;
-            shortcut.Save();
+            logger.LogError("Cannot determine working directory from executable path.");
+            return false;
         }
 
-        return true;
+        var linkPath = Path.Combine(startupPath, fileName + ".lnk");
+
+        try
+        {
+            // Create ShellLink instance
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            var shellLink = (IShellLinkW)new ShellLink();
+
+            // Set target path and working directory
+            shellLink.SetPath(executablePath);
+            shellLink.SetWorkingDirectory(originalFileDirectory);
+
+            // Save the shortcut
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            var persistFile = (IPersistFile)shellLink;
+            persistFile.Save(linkPath, true);
+
+            // Release COM objects
+            Marshal.ReleaseComObject(persistFile);
+            Marshal.ReleaseComObject(shellLink);
+
+            logger.LogInformation("Shortcut created successfully at: {LinkPath}", linkPath);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to create shortcut at: {LinkPath}", linkPath);
+            return false;
+        }
     }
+}
+
+// Native P/Invoke interfaces for creating Windows shortcuts without COM
+[ComImport]
+[Guid("000214F9-0000-0000-C000-000000000046")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+file interface IShellLinkW
+{
+    void GetPath([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, IntPtr pfd, uint fFlags);
+    void GetIDList(out IntPtr ppidl);
+    void SetIDList(IntPtr pidl);
+    void GetDescription([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName, int cchMaxName);
+    void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+    void GetWorkingDirectory([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
+    void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+    void GetArguments([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
+    void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+    void GetHotkey(out short pwHotkey);
+    void SetHotkey(short wHotkey);
+    void GetShowCmd(out int piShowCmd);
+    void SetShowCmd(int iShowCmd);
+    void GetIconLocation([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath, int cchIconPath, out int piIcon);
+    void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
+    void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, uint dwReserved);
+    void Resolve(IntPtr hwnd, uint fFlags);
+    void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
+}
+
+[ComImport]
+[Guid("0000010b-0000-0000-C000-000000000046")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+file interface IPersistFile
+{
+    void GetClassID(out Guid pClassID);
+    [PreserveSig]
+    int IsDirty();
+    void Load([MarshalAs(UnmanagedType.LPWStr)] string pszFileName, uint dwMode);
+    void Save([MarshalAs(UnmanagedType.LPWStr)] string? pszFileName, [MarshalAs(UnmanagedType.Bool)] bool fRemember);
+    void SaveCompleted([MarshalAs(UnmanagedType.LPWStr)] string pszFileName);
+    void GetCurFile([MarshalAs(UnmanagedType.LPWStr)] out string ppszFileName);
+}
+
+[ComImport]
+[Guid("00021401-0000-0000-C000-000000000046")]
+[ClassInterface(ClassInterfaceType.None)]
+file class ShellLink
+{
 }
