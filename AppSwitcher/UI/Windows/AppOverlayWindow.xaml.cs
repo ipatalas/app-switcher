7632b7857943b66f76d46c041ddcfa87843050e0
Ireneusz Patalas <fido@list.pl>
2026-03-27 15:18:13 +0100

    Add dynamic shadow effect to overlay card based on theme

--- AppSwitcher/UI/Windows/AppOverlayWindow.xaml.cs
+++ AppSwitcher/UI/Windows/AppOverlayWindow.xaml.cs
@@ -1,5 +1,8 @@
 using AppSwitcher.UI.ViewModels;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using Wpf.Ui.Appearance;
 
 namespace AppSwitcher.UI.Windows;
 
@@ -12,10 +15,32 @@ public partial class AppOverlayWindow : Window
 
         Loaded += OnLoaded;
         SizeChanged += OnSizeChanged;
+        Closed += OnClosed;
+
+        ApplicationThemeManager.Changed += OnThemeChanged;
     }
 
-    private void OnLoaded(object sender, RoutedEventArgs e) => PositionWindow();
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        ApplyShadowForTheme();
+        PositionWindow();
+    }
+
     private void OnSizeChanged(object sender, SizeChangedEventArgs e) => PositionWindow();
+
+    private void OnClosed(object? sender, EventArgs e) =>
+        ApplicationThemeManager.Changed -= OnThemeChanged;
+
+    private void OnThemeChanged(ApplicationTheme theme, System.Windows.Media.Color _) => ApplyShadowForTheme();
+
+    private void ApplyShadowForTheme()
+    {
+        var isDark = ApplicationThemeManager.GetAppTheme() == ApplicationTheme.Dark;
+
+        CardBorder.Effect = isDark
+            ? new DropShadowEffect { BlurRadius = 20, ShadowDepth = 0, Opacity = 0.25, Color = Colors.White }
+            : new DropShadowEffect { BlurRadius = 24, ShadowDepth = 8, Direction = 270, Opacity = 0.4, Color = Colors.Black };
+    }
 
     private void PositionWindow()
     {
